5d4298065fd41305f9e9cf99993907ebaddf3b9f
Priyanka-Barole <priyankabarole888@gmail.com>
2024-01-15 19:40:44 +0530

    added order number

---
+++ order_number.py
@@ -0,0 +1,27 @@
+
+#write a python script enter the order number user difinend one value.
+#input:enter integer order number.
+#process:take a value user difinend value
+# 1.if order number equal equal 1 : print"you have order pizz"
+# 2.elif order number equal equal 2 :print "you have order burger"
+# 3.elif order number equal equal 3 :print "you have order vada pav"
+# 4.elif order number equal equal 4 :print "you have order kachori"
+# 5.elif order number equal equal 5 :print "you have order close"
+
+
+order_number=int(input("enter the order number:"))
+if order_number == 1:
+
+	print("you have order pizz")
+elif order_number == 2:
+	print("you have order burger")
+elif order_number == 3:
+	print("you have order vada pav")
+elif order_number == 4:
+	print("you have order kachori")
+elif order_number == 5:
+	print("you have order close")
+
+
+
+
