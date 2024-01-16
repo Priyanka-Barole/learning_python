1c236f0b3cd5570207b182675451f0bbd11d24e8
Priyanka-Barole <priyankabarole888@gmail.com>
2024-01-16 18:52:32 +0530

    Update area of carcl

---
+++ area_of_carcl.py
@@ -0,0 +1,13 @@
+# Que- Write a python program find area of circle.
+# solution-
+# Input-radius of circle
+# output-area of circle
+# process-
+# 1) take a user difiend value radius of circle and  allocated its value in radius_of_circle.
+# 2) Find area of circle by 22/7*(radius_of_circle)**2 and allocate its value in area_of_circle.
+# 3) print area_of_circle.
+
+var_radius_circle = int(input("Enter value of radius of circle:"))
+result_area_of_circle = 22/7*(var_radius_circle**2)
+print(result_area_of_circle)
+
