0dc01ca1afecd1cc4ca2f9fe37c57d250c6b3a72
Priyanka-Barole <priyankabarole888@gmail.com>
2024-01-20 23:04:50 +0530

    update city

---
+++ city.py
@@ -0,0 +1,12 @@
+# Que- write a python program acepte  any city disply momment that city.
+# solution-
+
+city=int(input("enter name of the city:"))
+if lowar.city== "Delhi":
+	print("momument name :red fort")
+elif lowar.city=="agra":
+	print("momument name :taj mahal")
+elif lowar.city=="jipur":
+	print("momument name :jal mahal")
+else:
+	print("enter creat name of city")
