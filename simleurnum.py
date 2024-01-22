59ca22cd5fadc98810012143227eb840d899fec4
Priyanka-Barole <priyankabarole888@gmail.com>
2024-01-22 21:49:04 +0530

    update simleurnum

---
+++ simleurnum.py
@@ -0,0 +1,8 @@
+# write a python program enter frist and secend number if a greater number secend samller else number frist samller.
+# solution-
+num1=int(input("enter the number frist:"))
+num2=int(input("enter the number secend:"))
+if num1>num2:
+	print("samller number is",num2)
+else:
+	print("samller number is",num1)
