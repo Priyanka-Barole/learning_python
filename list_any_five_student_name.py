99ba0ff9d9a0227d8283fe6404c61bffb0dcb086
Priyanka-Barole <priyankabarole888@gmail.com>
2024-01-23 22:01:28 +0530

    update any five student name and any five number list

---
+++ list_any_five_student_name.py
@@ -0,0 +1,18 @@
+# Que-write a python program to create a follwing list(take input form the user)
+# any five student name
+# solution-
+
+lst=[]
+student_name=input("enter student name:")
+for i in range(5):
+	lst.append(student_name)
+print(lst)
+
+# any five number
+# solution-
+
+lst=[]
+num=input("enter number:")
+for i in range(5):
+	lst.append(num)
+print(lst)	
