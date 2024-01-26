293e888fad2afdd0087e44d939fb861c2b455e8b
Priyanka-Barole <priyankabarole888@gmail.com>
2024-01-26 21:27:23 +0530

    update string file

---
+++ string.py
@@ -0,0 +1,13 @@
+# Que-print given string 
+'''_str="I am Priyanka"
+_str2=""
+for i in range(len(_str)):
+	_str2+=_str[i]
+print(_str2)
+'''
+# Que-print given string
+_str="I am Priyanka"
+_str2=""
+for i in range(len(_str)):
+	_str2+=_str[i]
+	print(i*_str[i])	
