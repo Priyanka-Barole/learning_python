184f2460fd7432e1ed6df9f8009a7e59d3adda08
Priyanka-Barole <priyankabarole888@gmail.com>
2024-01-29 17:23:42 +0530

    join two name function

---
+++ join_two_name_function.py
@@ -0,0 +1,9 @@
+# Que- write a python program user defind function join_name()that accept thr frist name and last name as argument and concatrnate them to get full name.
+# solition:-
+def join_name(x1,y1):
+	fullname=x1+y1
+	print(fullname)
+x1name=(input("enter frist name:"))
+y1name=(input("enter secend name:"))
+join_name(x1name,y1name)
+
