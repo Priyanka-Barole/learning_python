0e394fbde032e26b96c8b022d95db95b1cc44d2d
Priyanka-Barole <priyankabarole888@gmail.com>
2024-01-30 18:29:09 +0530

    update argument square function

---
+++ argument_square_function.py
@@ -0,0 +1,5 @@
+# write a user difined fuction "sqnum" which accept an intiger as an argument and display it s square.
+def squnm(num1):
+	print("square of a number",num1*num1)
+n1=int(input("enter any number:"))
+print(n1)	
