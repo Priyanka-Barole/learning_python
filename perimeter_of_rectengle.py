d1bd476378094071c68622791a4aca71a54165e5
Priyanka-Barole <priyankabarole888@gmail.com>
2024-01-31 18:07:44 +0530

    update perimeter of rectengle

---
+++ perimeter_of_rectengle.py
@@ -0,0 +1,7 @@
+# Que-Write a python program to calculate perimeter of rectengle.
+# solution-
+
+var_rectengle_of_length =int (input("enter value of length of rectengle:"))
+var_rectengle_of_width = int (input("enter value of width  of rectengle:"))
+result_perimeter_of_rectengle = 2*(var_rectengle_of_length+var_rectengle_of_width)
+print(result_perimeter_of_rectengle)
