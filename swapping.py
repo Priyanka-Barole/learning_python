4eee42cedd68f282a5f42ee0a6f86580fd8af6aa
Priyanka-Barole <priyankabarole888@gmail.com>
2024-01-31 18:13:03 +0530

    add swapping

---
+++ swapping.py
@@ -0,0 +1,33 @@
+# swapping-
+# 1 method-
+# solution-
+
+a=input("enter frist no:")
+b=input("enter secend no:")
+print(a,b)
+temp=a
+a=b
+b=temp
+print(a,b)
+
+
+# swapping-
+# 2 method-
+# solution-
+
+a=input("enter frist number:")
+b=input("enter secend number:")
+print(a,b)
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+
+# swepping simple method-
+# solution-
+a=input("enter frist number:")
+b=input("enter frist number:")
+print(a,b)
+a,b=b,a   # a to value b i will go and b to value a i will go
+print(a,b)
+
