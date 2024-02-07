fc013c258de429d370bb51e22e15da02819f3d4d
Priyanka-Barole <priyankabarole888@gmail.com>
2024-02-07 12:50:12 +0530

    update factorial use to forloop

---
+++ loop/find_factorial_use_forloop.py
@@ -0,0 +1,8 @@
+# que-write a python program find the fecactoral of number.
+# solution-
+
+var_x=int(input("enter a value:"))
+result=1
+for i in range(1,var_x+1):
+	result=result*i
+print(result) 
