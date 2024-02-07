082d5945b85b2f043f9b89d0349708bd30798d79
Priyanka-Barole <priyankabarole888@gmail.com>
2024-02-07 12:34:18 +0530

    added pattern for loop

---
+++ loop/pattern_1.py
@@ -0,0 +1,20 @@
+# Que-write a python program print given pattern.
+# for  n=3   for  n=4
+# *****
+# *****
+# *****
+
+
+# Que-write a python program print given pattern.
+# for n=3  for=4
+# *****    *****
+# *****    *****  
+# *****    *****
+n=int(input("enter the n:"))
+for _ in range(n):
+	print("*" * 5)
+
+
+
+
+
