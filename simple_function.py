a2e3fe2374c69c5ad92cb7c20258748949791437
Priyanka-Barole <priyankabarole888@gmail.com>
2024-02-12 17:48:58 +0530

    added simple function

---
+++ simple_function.py
@@ -0,0 +1,9 @@
+# Que-write a python program tow number addition and subtraction use to function.
+# solution-
+def calculation(a,b):
+
+	addition=a+b
+	subtraction=a-b
+	return addition,subtraction
+result=calculation(30,20)
+print(result)	
