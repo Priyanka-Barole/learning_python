76e9bfd490f7727ce7465046992f7410bead771a
Priyanka-Barole <priyankabarole888@gmail.com>
2024-02-14 17:00:22 +0530

    update vowels

---
+++ print_vowles.py
@@ -0,0 +1,7 @@
+# Que-write a python program create tow list print vowels.
+# solution-
+lst_1=['A','B','C','D','E','F','G','H','I','J','K','L']
+lst_2=['a','e','i','o','u']
+for i in lst_2:
+ 	lst_1.append(i)
+print(lst_1[0],lst_1[4],lst_1[8])
